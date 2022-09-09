a110fb762111ad567b851a56c34593c38cb009b5
Teringer Alex <ateringer@b2international.com>
2022-09-09 17:02:54 +0200

    feat(change): add new props

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,37 @@
-import styles from '../styles/Card.module.css'
-import React from 'react'
+import styles from "../styles/Card.module.css";
+import Date from "../components/FormatDate";
+import mdIcon from "../public/md_icon.svg";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
-interface CardProps{
-    children: JSX.Element
+interface CardProps {
+  id: string;
+  title: string;
+  date: string;
+  fileSize: number[]
 }
 
-const Card = ({children}: CardProps) => {
+const Card = ({ title, date, id, fileSize }: CardProps) => {
   return (
     <div className={styles.card_body}>
-    {children}
+      <div className={styles.titleAndIcon}>
+        <Link  href={`/files/${id}`}>
+          <h3 className={styles.link}>{title}</h3>
+        </Link>
+        <Image height={20} width={24} className={styles.title_image} src={mdIcon} alt="markdown" title="markdown" />
+      </div>
+      <small className={styles.lightText}>
+        <Date dateString={date} />
+        {fileSize}
+      </small>
+      <p className={styles.text_container}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
+        inventore error totam voluptatem voluptatibus, illum ipsam rem provident
+        optio adipisci, quasi ullam. Dolore, fuga sit.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
