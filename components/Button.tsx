3c2d560277a8b7ccf66eb4afc65a270853166500
Teringer Alex <ateringer@b2international.com>
2022-09-14 17:48:25 +0200

    feat(new): create button component

---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from '../styles/Button.module.css'
+import {classNames} from '../utiles/className'
+import Image, { StaticImageData } from 'next/image'
+
+interface ButtonProps {
+  text: string;
+  icon: StaticImageData,
+  className: string,
+  onClick: React.MouseEventHandler
+}
+
+const Button = ({ text, icon, className, onClick }: ButtonProps) => {
+  return (
+    <button className={classNames(`${styles.btn} ${className}`)} onClick={onClick}>
+        <div>
+            {text}
+        </div>
+      <Image width={14} height={14} src={icon} alt="actionButton"/>
+    </button>
+  );
+};
+
+export default Button;
