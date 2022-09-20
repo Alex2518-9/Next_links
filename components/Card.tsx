2b8c7c5ea80f3415866ae8af90ffc12e8c1136cd
Teringer Alex <ateringer@b2international.com>
2022-09-20 17:44:41 +0200

    fix(onClick): use onClick

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,38 +1,46 @@
 import styles from "../styles/Card.module.css";
 import Date from "../components/FormatDate";
 import mdIcon from "../public/md_icon.svg";
+import txtIcon from "../public/txt_icon.svg";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { File } from "../pages/api/getFiles";
 
-
 export interface CardProps extends File {
   onClick: React.MouseEventHandler;
-
 }
 
-const Card = ({ title, date, id, fileSize, onClick }: CardProps) => {
+const Card = ({ title, date, id, fileSize, onClick, extension }: CardProps) => {
   return (
-    <li
-      className={styles.standard}
-      onClick={onClick}
-    >
+    <li className={styles.standard} onClick={onClick}>
       <div className={styles.titleContainer}>
         <div className={styles.titleAndIcon}>
           <Link href={`/files/${id}`}>
             <h3 className={styles.link}>{title}</h3>
           </Link>
-          <Image
-            height={20}
-            width={24}
-            className={styles.title_image}
-            src={mdIcon}
-            alt="markdown"
-            title="markdown"
-          />
+          {extension === ".md" ? (
+            <Image
+              height={20}
+              width={24}
+              className={styles.title_image}
+              src={mdIcon}
+              alt="markdown"
+              title="markdown"
+            />
+          ) : (
+            extension === ".txt" && (
+              <Image
+                height={20}
+                width={24}
+                className={styles.title_image}
+                src={txtIcon}
+                alt="markdown"
+                title="markdown"
+              />
+            )
+          )}
         </div>
-      
       </div>
       <small className={styles.lightText}>
         <Date dateString={date} />
